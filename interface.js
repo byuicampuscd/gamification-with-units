4297730ce1f0cd4c07c2182a000ab1e445d854a2
Grant Merrill <grant.merrill@live.com>
2016-09-01 17:03:46 -0600

    Began the logic for calculating the unit grades

--- interface.js
+++ interface.js
@@ -25,12 +25,70 @@
             }
  */
 
+function findCategory(gradeItem) {
+
+}
+
 valence.run(function(err, data) {
     var nameOfStudent,
         context,
         categories,
+        validCategories = {
+            Unit1Required: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit1Optional: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit2Required: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit2Optional: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit3Required: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit3Optional: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit4Required: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit4Optional: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit5Required: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit5Optional: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit6Required: {
+                numerator: 0,
+                denominator: 0
+            },
+            Unit6Optional: {
+                numerator: 0,
+                denominator: 0
+            }
+        },
+        grades,
         i,
-        categoryRegex,
+        catIndex,
+        reqRegex = /(?=.*unit)(?=.*required)(?=.*\d).*/ig,
+        optRegex = /(?=.*unit)(?=.*optional)(?=.*\d).*/ig,
+        numRegex,
         tempUnits = [],
         unitTemplate = {
             requiredPer: 0,
@@ -50,29 +108,40 @@ valence.run(function(err, data) {
 
         // Go through the different categories and assign them to the proper units
         categories = data.getCategories();
-        for (i = 0; i < categories.length; i++) {
-            categoryRegex = /(?=.*unit)(?=.*required)(?=.*\d).*/ig;
+        //		for (i = 0; i < categories.length; i++) {
+        //                  if (categories[i].catName.search(reqRegex) !== -1) {
+        //                    else {
+        //                      if (categories[i].catName.search(optRegex) !== -1) {
+        //                        tempUnits.push({
+        //                          type: "optional",
+        //                        name: categories[i].catName,
+        //                      id: categories[i].catID
+        //                });
+        //          } else {
+        //            // Should this do anything or just simply ignore the category?
+        //                       }
+        //                 }
+        //           }
 
-            if (categories[i].catName.search(categoryRegex) !== -1) {
-                tempUnits.push({
-                    type: "required",
-                    name: categories[i].catName
-                });
-            } else {
-                categoryRegex = /(?=.*unit)(?=.*optional)(?=.*\d).*/ig;
+        // Go through the grades
+        grades = data.getGrades();
+        for (i = 0; i < grades.length; i++) {
+            // Find category associative index (name of category)
+            catIndex = findCategory(grades[i]);
 
-                if (categories[i].catName.search(categoryRegex) !== -1) {
-                    tempUnits.push({
-                        type: "optional",
-                        name: categories[i].catName
-                    });
+            if (catIndex !== "") {
+                // Add up the numerators and denominators
+                if (grades[i].weightedNumerator !== null) {
+                    validCategories[catIndex].numerator += grades[i].weightedNumerator;
+                    validCategories[catIndex].denominator += grades[i].weightedDenominator;
                 } else {
-                    // Should this do anything or just simply ignore the category?
+                    validCategories[catIndex].numerator += grades[i].pointsNumerator;
+                    validCategories[catIndex].denominator += grades[i].pointsDenominator;
                 }
             }
         }
 
-        // Go through the grades and begin to calculate the percentages for the unit
+        // Sort validCategories by UnitNumber
     } else {
         console.log("ERROR");
     }
