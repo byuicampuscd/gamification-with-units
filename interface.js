9ee208df44c801810b10f1b6e43a1d3916e39444
Grant Merrill <grant.merrill@live.com>
2016-09-01 14:56:14 -0600

    Added logic for going through the categories

--- interface.js
+++ interface.js
@@ -1,4 +1,4 @@
-/* jslint browser: true */
+/* jslint plusplus: true, browser: true */
 /* global valence.js */
 
 /* Reminder:
@@ -26,11 +26,53 @@
  */
 
 valence.run(function(err, data) {
-    var nameOfStudent;
+    var nameOfStudent,
+        context,
+        categories,
+        i,
+        categoryRegex,
+        tempUnits = [],
+        unitTemplate = {
+            requiredPer: 0,
+            optionalPer: 0,
+            unitGrade: ""
+        };
 
-    if (err !== null) {
+    if (err === null) {
         console.log("No error");
+
+        // Get the name of the student
         nameOfStudent = data.getFirstName() + " " + data.getLastName();
+        context = {
+            studentName: nameOfStudent,
+            units: []
+        };
+
+        // Go through the different categories and assign them to the proper units
+        categories = data.getCategories();
+        for (i = 0; i < categories.length; i++) {
+            categoryRegex = /(?=.*unit)(?=.*required)(?=.*\d).*/ig;
+
+            if (categories[i].catName.search(categoryRegex) !== -1) {
+                tempUnits.push({
+                    type: "required",
+                    name: categories[i].catName
+                });
+            } else {
+                categoryRegex = /(?=.*unit)(?=.*optional)(?=.*\d).*/ig;
+
+                if (categories[i].catName.search(categoryRegex) !== -1) {
+                    tempUnits.push({
+                        type: "optional",
+                        name: categories[i].catName
+                    });
+                } else {
+                    // Should this do anything or just simply ignore the category?
+                }
+            }
+        }
+
+        // Go through the grades and begin to calculate the percentages for the unit
     } else {
         console.log("ERROR");
     }
