ab12aa2240be57efc4216769c8efb3b2a2120aa9
Joshua McKinney <meeple142@users.noreply.github.com>
2016-09-02 13:03:44 -0600

    handelbarsContextPattern.json is now a js file and has var

---
+++ handelbarsContextPattern.js
@@ -0,0 +1,14 @@
+var context = {
+    studentName: "First Last",
+    units: [
+        {
+            requiredPer: 30,
+            optionalPer: 41,
+            unitGrade: "B+"
+      }, {
+            requiredPer: 0,
+            optionalPer: 0,
+            unitGrade: ""
+      }
+   ]
+}
