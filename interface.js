26e2602b8c2c0d7b218e94227cb27e35770a26e5
Grant Merrill <grant.merrill@live.com>
2016-09-08 19:40:12 -0600

    Added logic for getting the required and optional percentages of the different units

--- interface.js
+++ interface.js
@@ -25,120 +25,63 @@
             }
  */
 
-function findCategory(gradeItem) {
-
-}
-
 valence.run(function(err, data) {
-    var nameOfStudent,
-        context,
-        categories,
-        validCategories = {
-            Unit1Required: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit1Optional: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit2Required: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit2Optional: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit3Required: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit3Optional: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit4Required: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit4Optional: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit5Required: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit5Optional: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit6Required: {
-                numerator: 0,
-                denominator: 0
-            },
-            Unit6Optional: {
-                numerator: 0,
-                denominator: 0
-            }
+    "use strict";
+
+    var nameOfStudent = data.getFirstName() + " " + data.getLastName(),
+        context = {
+            studentName: nameOfStudent,
+            units: []
         },
-        grades,
+        categories = data.getCategories(),
+        grades = data.getGrades(),
         i,
-        catIndex,
-        reqRegex = /(?=.*unit)(?=.*required)(?=.*\d).*/ig,
-        optRegex = /(?=.*unit)(?=.*optional)(?=.*\d).*/ig,
-        numRegex,
-        tempUnits = [],
-        unitTemplate = {
-            requiredPer: 0,
-            optionalPer: 0,
-            unitGrade: ""
-        };
+        catName,
+        tempUnits = [];
+
+    function findCategory(gradeItem) {
+        "use strict";
+        var i;
+        for (i = 0; i < categories.length; ++i) {
+            if (categories[i].catID === gradeItem.catID) {
+                return categories[i].catName;
+            }
+        }
+
+        return "";
+    }
 
     if (err === null) {
         console.log("No error");
 
-        // Get the name of the student
-        nameOfStudent = data.getFirstName() + " " + data.getLastName();
-        context = {
-            studentName: nameOfStudent,
-            units: []
-        };
+        // Go through the different grades
+        for (i = 0; i < grades.length; ++i) {
+            catName = findCategory(grades[i]);
 
-        // Go through the different categories and assign them to the proper units
-        categories = data.getCategories();
-        //		for (i = 0; i < categories.length; i++) {
-        //                  if (categories[i].catName.search(reqRegex) !== -1) {
-        //                    else {
-        //                      if (categories[i].catName.search(optRegex) !== -1) {
-        //                        tempUnits.push({
-        //                          type: "optional",
-        //                        name: categories[i].catName,
-        //                      id: categories[i].catID
-        //                });
-        //          } else {
-        //            // Should this do anything or just simply ignore the category?
-        //                       }
-        //                 }
-        //           }
-
-        // Go through the grades
-        grades = data.getGrades();
-        for (i = 0; i < grades.length; i++) {
-            // Find category associative index (name of category)
-            catIndex = findCategory(grades[i]);
-
-            if (catIndex !== "") {
-                // Add up the numerators and denominators
-                if (grades[i].weightedNumerator !== null) {
-                    validCategories[catIndex].numerator += grades[i].weightedNumerator;
-                    validCategories[catIndex].denominator += grades[i].weightedDenominator;
-                } else {
-                    validCategories[catIndex].numerator += grades[i].pointsNumerator;
-                    validCategories[catIndex].denominator += grades[i].pointsDenominator;
-                }
+            // Check if the Unit already exists or not
+            if (tempUnits[catName] === undefined) {
+                tempUnits[catName] = { numerator: 0, denominator: 0 };
             }
+
+            // Check if grade is graded
+            if (grades[i].weightedNumerator !== null) {
+                tempUnits[catName].numerator += grades[i].weightedNumerator;
+                tempUnits[catName].denominator += grades[i].weightedDenominator;
+            } else if (grades[i].pointsNumerator !== null) {
+                tempUnits[catName].numerator += grades[i].pointsNumerator;
+                tempUnits[catName].denominator += grades[i].pointsDenominator;
+            }
+        }
+
+        // Add the unit to the context
+        for (i = 0; i < tempUnits.length; ++i) {
+            context.units.push({
+                requiredPer: tempUnits["Unit" + parseInt(i + 1) + "Required"].numerator /
+                    tempUnits["Unit" + parseInt(i + 1) + "Required"].denominator,
+                optionalPer: tempUnits["Unit" + parseInt(i + 1) + "Optional"].numerator /
+                    tempUnits["Unit" + parseInt(i + 1) + "Optional"].denominator,
+                unitGrade: ""
+            });
         }
     } else {
         console.log("ERROR");
