322318a517d57e5a69f8d3ca6b6e89c9be21d84f
Grant Merrill <grant.merrill@live.com>
2016-09-28 13:41:08 -0600

    Added beginning logic to handle the construction of the context

--- test.js
+++ test.js
@@ -2,11 +2,51 @@
 /*global scormSuspendData */
 
 // This is currently only the Student Workflow
-var data = scormSuspendData.getData();
-if (data === '') {
-    // First Time
-    console.log("First Time");
-} else {
-    // Subsequent Times
-    console.log("Subsequent Times");
-}
+var callback = function (data) {
+    "use strict";
+    //var scormData = scormSuspendData.getData(),
+    var i,
+        j,
+        context = {
+            nameOfStudent: data.getFirstName() + " " + data.getLastName(),
+            courseName: data.getCourseName(),
+            currentChosenGrade: "",
+            categories: null,
+            optionalGrades: []
+        },
+        // TODO(Grant): Loop through cats. Add properties to each cat:  numerator total and denominator of one item. Do this with optionalGrades.
+        tempCats = data.getCategories();
+
+
+    /* NOTE(Grant): This will NOT work right now! Either I need to loop through the grades or
+                    make the categories know more about their grade items. */
+    for (i = 0; i < tempCats.length; ++i) {
+        tempCats[i].finalNumerator = 0;
+        tempCats[i].finalDenominator = 0;
+        for (j = 0; j < tempCats[i].grades.length; ++j) {
+            // NOTE(Grant): There is no 'else' here.
+            // Add up all the numerators
+            if (tempCats[i].grades[j].weightedNumerator !== null) {
+                tempCats[i].finalNumerator += tempCats[i].grades[j].weightedNumerator;
+            } else if (tempCats[i].grades[j].pointsNumerator !== null) {
+                tempCats[i].finalNumerator += tempCats[i].grades[j].pointsNumerator;
+            }
+
+            // Get the first grade's denominator
+            if (tempCats[i].finalDenominator === 0 && tempCats[i].grades[j].weightedDenominator !== null) {
+                tempCats[i].finalDenominator = tempCats[i].grades[j].weightedDenominator;
+            } else if (tempCats[i].finalDenominator === 0 && tempCats[i].grades[j].pointsDenominator !== null) {
+                tempCats[i].finalDenominator = tempCats[i].grades[j].pointsDenominator;
+            }
+        }
+    }
+
+    // NOTE(Grant): This is 
+    /*if (scormData === '') {
+        // First Time
+        console.log("First Time");
+    } else {
+        // Subsequent Times
+        console.log("Subsequent Times");
+    }*/
+};
